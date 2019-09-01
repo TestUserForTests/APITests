6d06a3f3f455c25c4771b0c77f86495ea5ee0dff
Anton Azarnin <AAzarnin@luxoft.com>
2019-09-01 16:34:34 +0300

    final variant

--- src/test/java/ru/mosExchange/testProject/IssueTests.java
+++ src/test/java/ru/mosExchange/testProject/IssueTests.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
-import org.apache.http.client.fluent.Response;
 import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,66 +13,61 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class IssueTests {
+class IssueTests {
 
-    private static final String TOKEN = "93f56cd3a131bc66ecee5e232d4a02ee091fbc7e";
+    private static final String TOKEN = "db41fc4d5367e2e3cb604dce2ba2b1c4645e1ef3";
     private static final String URL = "https://api.github.com/repos/TestUserForTests/APITests/issues";
     private static Gson gson = new Gson();
-
-    private String issueTitle;
+    private String issueTitle = "Test Issue"+ UUID.randomUUID().toString();
     private Issue testIssue;
-    private Collection<Issue> setOfIssues;
-
+    private Collection<Issue> oldIssues;
     private TypeToken<List<Issue>> itemsListType = new TypeToken<>(){};
 
     @BeforeEach
-    public void init() throws IOException {
-        issueTitle = "Test Issue "+ UUID.randomUUID().toString();
+    void init() throws IOException {
         testIssue = new Issue().withTitle(issueTitle).withBody("Test Issue Body");
-        setOfIssues = getIssues(URL, itemsListType);
+        oldIssues = getIssues(itemsListType);
     }
 
     @Test
-    public void testIssueCreated() throws IOException {
-        System.out.println("Old set of issues: ");
-        setOfIssues.forEach(System.out::println);
+    void testIssueCreated() throws IOException {
+        System.out.println("Old set of user's issues: "+oldIssues.size()+" issues");
+        oldIssues.forEach(System.out::println);
 
         createIssue(testIssue);
 
-        System.out.println("\n" +"New set of issues: ");
-        setOfIssues.forEach(System.out::println);
+        Collection<Issue> newIssues = getIssues(itemsListType);
+        System.out.println("\n"+"New set of user's issues: "+ newIssues.size()+" issues");
+        newIssues.forEach(System.out::println);
 
-        assertEquals(1L, issueCountByExample(setOfIssues, testIssue), "One created issue expected");
-        System.out.println();
+        assertEquals(1L, issueCountByExample(newIssues, testIssue),"only one issue expected");
+        System.out.println("\n"+"Verification OK");
     }
 
-    // getting set of user's issues
-    private <T> T getIssues(String url,TypeToken<T> returnType) throws IOException {
-        String json = Request.Get(url)
+
+    private <T> T getIssues(TypeToken<T> returnType) throws IOException {
+        String json = Request.Get(IssueTests.URL)
                 .execute().returnContent().asString();
         return gson.fromJson(json,returnType.getType());
     }
 
-    //creation of an issue
     private void createIssue(Issue newIssue) throws IOException {
         String json = gson.toJson(newIssue);
-        post(URL, TOKEN,json);
+        post(json);
     }
 
-    //
-    private Response post(String url, String auth, String postData) throws IOException {
-        Request putRequest = Request.Post(url)
-                .addHeader("Authorization", "token "+ auth)
+    private void post(String postData) throws IOException {
+        Request putRequest = Request.Post(IssueTests.URL)
+                .addHeader("Authorization", "token "+ IssueTests.TOKEN)
                 .addHeader("Accept","application/vnd.github.symmetra-preview+json")
                 .bodyString(postData, ContentType.APPLICATION_JSON);
-        return getExecutor().execute(putRequest);
+        getExecutor().execute(putRequest);
     }
 
     private Executor getExecutor() {
         return Executor.newInstance();
     }
 
-    //getting the number of created issues
     private long issueCountByExample(Collection<Issue> issues, Issue example){
         return issues.stream().filter(issue -> example.getTitle().equals(issue.getTitle())).count();
     }
