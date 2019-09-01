7aeeb74730b26c3a4a70bf90c2bf9d47943e3aa1
Anton Azarnin <AAzarnin@luxoft.com>
2019-09-01 13:43:46 +0300

    working variant without timeout after issue creation

--- src/test/java/ru/mosExchange/testProject/IssueTests.java
+++ src/test/java/ru/mosExchange/testProject/IssueTests.java
@@ -1,69 +1,80 @@
 package ru.mosExchange.testProject;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.fluent.Response;
+import org.apache.http.entity.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IssueTests {
 
+    private static final String TOKEN = "93f56cd3a131bc66ecee5e232d4a02ee091fbc7e";
+    private static final String URL = "https://api.github.com/repos/TestUserForTests/APITests/issues";
+    private static Gson gson = new Gson();
+
+    private String issueTitle;
+    private Issue testIssue;
+    private Collection<Issue> setOfIssues;
+
+    private TypeToken<List<Issue>> itemsListType = new TypeToken<>(){};
+
+    @BeforeEach
+    public void init() throws IOException {
+        issueTitle = "Test Issue "+ UUID.randomUUID().toString();
+        testIssue = new Issue().withTitle(issueTitle).withBody("Test Issue Body");
+        setOfIssues = getIssues(URL, itemsListType);
+    }
+
     @Test
-    public void testCreateIssue() throws IOException {
-        Set<Issue> oldIssues = getIssues();
-        Issue newIssue = new Issue().withTitle("Test Issue").withBody("Test Issue Body");
-        int IssueId = createIssue(newIssue);
-        Set<Issue> newIssues = getIssues();
-        oldIssues.add(newIssue.withId(IssueId));
-        assertEquals(newIssues, oldIssues);
+    public void testIssueCreated() throws IOException {
+        System.out.println("Old set of issues: ");
+        setOfIssues.forEach(System.out::println);
+
+        createIssue(testIssue);
+
+        System.out.println("\n" +"New set of issues: ");
+        setOfIssues.forEach(System.out::println);
+
+        assertEquals(1L, issueCountByExample(setOfIssues, testIssue), "One created issue expected");
+        System.out.println();
     }
 
-    private Set<Issue> getIssues() throws IOException {
-        String json = Request.Get("https://api.github.com/repos/TestUserForTests/APITests/issues")
+    // getting set of user's issues
+    private <T> T getIssues(String url,TypeToken<T> returnType) throws IOException {
+        String json = Request.Get(url)
                 .execute().returnContent().asString();
-        return new HashSet<>(Arrays.asList((new Gson()).fromJson(json,Issue[].class)));
+        return gson.fromJson(json,returnType.getType());
     }
 
-    private int createIssue(Issue newIssue) throws IOException {
-        String json = Request.Post("https://api.github.com/repos/TestUserForTests/APITests/issues")
-                .addHeader("Authorization","51bd85e2f2a96d0d083c363ca6dfed33ec5455b9")
-                .bodyForm(new BasicNameValuePair("title", newIssue.getTitle()),
-                        new BasicNameValuePair("body", newIssue.getBody()))
-                .execute().returnContent().asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        return parsed.getAsJsonObject().get("id").getAsInt();
+    //creation of an issue
+    private void createIssue(Issue newIssue) throws IOException {
+        String json = gson.toJson(newIssue);
+        post(URL, TOKEN,json);
     }
 
-//    private Set<Issue> getIssues() throws IOException {
-//        String json = getExecutor().execute(Request.Get("https://api.github.com/repos/TestUserForTests/APITests/issues"))
-//                .returnContent().asString();
-//        JsonElement parsed = new JsonParser().parse(json);
-//        JsonElement issues = parsed.getAsJsonObject().get("issues");
-//        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-//        return new HashSet<>(Arrays.asList((new Gson()).fromJson(json,Issue[].class)));
-//    }
+    //
+    private Response post(String url, String auth, String postData) throws IOException {
+        Request putRequest = Request.Post(url)
+                .addHeader("Authorization", "token "+ auth)
+                .addHeader("Accept","application/vnd.github.symmetra-preview+json")
+                .bodyString(postData, ContentType.APPLICATION_JSON);
+        return getExecutor().execute(putRequest);
+    }
 
-//    private Executor getExecutor() {
-//        return Executor.newInstance()
-//                .auth("51bd85e2f2a96d0d083c363ca6dfed33ec5455b9", "");
-//    }
+    private Executor getExecutor() {
+        return Executor.newInstance();
+    }
+
+    //getting the number of created issues
+    private long issueCountByExample(Collection<Issue> issues, Issue example){
+        return issues.stream().filter(issue -> example.getTitle().equals(issue.getTitle())).count();
+    }
 }
